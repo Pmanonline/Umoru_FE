26a8ef48addad97ad85f4253a9eb523d9de2900a
peter <petersonzoconis@gmail.com>
2025-08-21 20:09:21 +0100

    fix: normalize casing for auth folder

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // const backendURL = import.meta.VITE_BACKEND_URL;
 
 // export default backendURL;
-const backendURL = "http://localhost:3001";
+// const backendURL = "http://localhost:3001";
+const backendURL = "https://umoru-be.vercel.app";
 
 export default backendURL;
